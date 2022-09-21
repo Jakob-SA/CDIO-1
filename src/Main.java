8b9a9d6450fa22f212fe05c4d34e1c126c6d95d1
Philip <philipmuff@gmail.com>
2022-09-21 11:40:45 +0200

    Tilfældige terninger tilføjet.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        java.lang.Math.random();
+        Scanner input = new Scanner(System.in);
+        input.next();
+
+        int tal1 =(int) (Math.random()*6+1);
+        int tal2 =(int) (Math.random()*6+1);
+        int sum = tal1 + tal2;
+
+        System.out.println(tal1);
+        System.out.println(tal2);
+        System.out.println("Summen er: " + sum);
+
+
+
+
+
+
+
     }
+
 }
