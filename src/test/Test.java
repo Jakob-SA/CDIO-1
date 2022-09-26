cff209edde9db1a3aaad0d31ff154f6a22449485
Esben Elnegaard <esbenskov@live.dk>
2022-09-26 14:49:22 +0200

    Version 4, test af terninger. Metoder tilkaldt i stedet for lokale metoder.

--- src/test/Test.java
+++ src/test/Test.java
@@ -1,18 +1,17 @@
 package test;
 
+import spil.Die;
+
 public class Test {
     public void test() {
-
-
-         int die1 = (int) (Math.random() * 6) + 1;
-         int die2 = (int) (Math.random() * 6) + 1;
+         spil.Die die1 = new Die();
+         spil.Die die2 = new Die();
 
          int freq[] = new int[14];
 
         for (int counter = 0; counter < 1000; counter++) {
-            die1 = (int) (Math.random() * 6) + 1;
-            die2 = (int) (Math.random() * 6) + 1;
-            ++freq[die1 + die2];
+
+            ++freq[die1.roll() + die2.roll()];
 
         }
         System.out.println("Face \t frequency");
