86919a4bcddf735824968d5e91d546ef3ac557c3
Jakob-SA <agergaardskovjakob@gmail.com>
2022-09-26 16:18:32 +0200

    Coinflip tilføjet

--- src/Main.java
+++ src/Main.java
@@ -1,41 +1,40 @@
-import spil.Die;
-import spil.Players;
-import spil.Points;
+import spil.*;
 
 import java.util.Scanner;
 
 
 public class Main {
     public static void main(String[] args) {
+        CoinFlip coinFlip = new CoinFlip();                 //bestemmer hvem der starter
+        coinFlip.fiftyFifty();
+
         Scanner input = new Scanner(System.in);
-        System.out.println("Skriv navn på spiller 1");
+        System.out.println("Skriv navn på spiller 1");          //tildeler navne til spillere
         Players Player1 = new Players(input.nextLine());
-
         System.out.println("Skriv navn på spiller 2");
-        Players Player2 = new Players(input.nextLine());              //tildeler navne til spillere
-
+        Players Player2 = new Players(input.nextLine());
 
         Die die1 = new Die();                               //opretter terninger
         Die die2 = new Die();
 
-        Points points1 = new Points(Player1);               //opretter spillere
+        Points points1 = new Points(Player1);               //opretter spillernes points
         Points points2 = new Points(Player2);
 
 
 
-        while (points1.points < 40) {
+        while (points1.points < 40 || points2.points < 40) {
             String tast = "q";
             while (tast != "") {
                 System.out.println("Tryk enter for at slå!");
                 tast = input.nextLine();
             }
-            System.out.println(die1.roll());
+
+            System.out.println(die1.roll());            //spiller 1 slår med terninger
             System.out.println(die2.roll());
 
+            points1.add(die1.getFaceValue(), die2.getFaceValue());          //lægger øjne til samlede point
 
-            points1.add(die1.getFaceValue(), die2.getFaceValue());
-
-            System.out.println(Player1.Navn + " Har " + points1.points + " points.");
+            System.out.println(Player1.Navn + " Har " + points1.points + " points.");       //printer akkumulerede points
 
             tast = "q";
             while (tast != "") {
@@ -43,13 +42,14 @@ public class Main {
                 tast = input.nextLine();
             }
 
-            System.out.println(die1.roll());
+            System.out.println(die1.roll());            //spiller 2 slår med terninger
             System.out.println(die2.roll());
 
-            points2.add(die1.getFaceValue(), die2.getFaceValue());
+            points2.add(die1.getFaceValue(), die2.getFaceValue());          //lægger øjne til samlede points
 
-            System.out.println(Player2.Navn + " Har " + points2.points + " points.");
+            System.out.println(Player2.Navn + " Har " + points2.points + " points.");       //printer akkumulerede points
         }
+
     }
 
 }
--- src/spil/CoinFlip.java
+++ src/spil/CoinFlip.java
@@ -1,15 +1,13 @@
 package spil;
-
 public class CoinFlip {
 
     private int value;
 
-    private void CoinFlip(){
-        value = (int)Math.round(Math.random());
+    public void fiftyFifty() {
+        value = (int) Math.round(Math.random());
         if (value == 0)
-            System.out.println();
+            System.out.println("Spiller 1 starter");
+        else System.out.println("Spiller 2 starter");
 
-
-        }
     }
-
+}
