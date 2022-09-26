d778ab75c8d34c38255eb3d51193ad4e9267611b
Esben Elnegaard <esbenskov@live.dk>
2022-09-26 14:39:42 +0200

    Version 3, test af terninger

--- src/test/Test.java
+++ src/test/Test.java
@@ -1,5 +1,29 @@
 package test;
 
 public class Test {
-    int as;
-}
+    public void test() {
+
+
+         int die1 = (int) (Math.random() * 6) + 1;
+         int die2 = (int) (Math.random() * 6) + 1;
+
+         int freq[] = new int[14];
+
+        for (int counter = 0; counter < 1000; counter++) {
+            die1 = (int) (Math.random() * 6) + 1;
+            die2 = (int) (Math.random() * 6) + 1;
+            ++freq[die1 + die2];
+
+        }
+        System.out.println("Face \t frequency");
+        for (int face = 1; face < freq.length; face++) {
+            System.out.println(face + "\t" + freq[face]);
+        }
+    }
+    }
+
+
+
+
+
+
