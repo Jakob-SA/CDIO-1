fe10176193416aa33baafe857ff5833e512cea3b
Jakob-SA <agergaardskovjakob@gmail.com>
2022-09-27 11:58:31 +0200

    Oprettet afslutning og redemption

--- src/Main.java
+++ src/Main.java
@@ -11,10 +11,10 @@ public class Main {
 
         Scanner input = new Scanner(System.in);
 
-        Players Player1 = new Players("a");
-        Players Player2 = new Players("b");
+        Players Player1 = new Players("Placeholder a");
+        Players Player2 = new Players("Placeholder b");
 
-        if (SpilStart.mont == 0) {
+        if (SpilStart.mont == 0) {                      //tildeler navne til spillere
             Player1.Navn = coinFlip.Spiller1Navn;
             Player2.Navn = coinFlip.Spiller2Navn;
         }else {
@@ -22,22 +22,17 @@ public class Main {
             Player2.Navn = coinFlip.Spiller1Navn;
         }
 
-
-                  //tildeler navne til spillere
-
-
-
-
         Die die1 = new Die();                               //opretter terninger
         Die die2 = new Die();
 
         Points points1 = new Points(Player1);               //opretter spillernes points
         Points points2 = new Points(Player2);
 
-        while (points1.points < 40 || points2.points < 40) {
+        while (points1.points < 40 && points2.points < 40) {
             String tast = "q";
             while (tast != "") {
-                System.out.println("Tryk enter for at slå!");
+                System.out.println("Det er " + Player1.Navn + "s tur.");
+                System.out.println("Tryk enter for at slå med terningerne!");
                 tast = input.nextLine();
             }
 
@@ -50,7 +45,8 @@ public class Main {
 
             tast = "q";
             while (tast != "") {
-                System.out.println("Tryk enter for at slå!");
+                System.out.println("Det er " + Player2.Navn + "s tur.");
+                System.out.println("Tryk enter for at slå med terningerne!");
                 tast = input.nextLine();
             }
 
@@ -60,6 +56,45 @@ public class Main {
             points2.add(die1.getFaceValue(), die2.getFaceValue());          //lægger øjne til samlede points
 
             System.out.println(Player2.Navn + " Har " + points2.points + " points.");       //printer akkumulerede points
+        }
+
+
+        if (points1.points > 39 && points2.points > 39) {                           //begge har over 40 points
+            System.out.println("I har begge over 40 point. Jeres næste slag afgøre det");
+
+            String tast = "q";
+            while (tast != "") {
+                System.out.println("Det er " + Player1.Navn + "s tur.");
+                System.out.println("Tryk enter for at slå med terningerne!");
+                tast = input.nextLine();
+            }
+            System.out.println(die1.roll());            //spiller 1 slår med terninger
+            System.out.println(die2.roll());
+
+            points1.redemption(die1.getFaceValue(), die2.getFaceValue());
+
+            System.out.println(Player1.Navn + " Har " + points1.points + " points.");       //printer akkumulerede points
+
+            tast = "q";
+            while (tast != "") {
+                System.out.println("Det er " + Player2.Navn + "s tur.");
+                System.out.println("Tryk enter for at slå med terningerne!");
+                tast = input.nextLine();
+            }
+
+            System.out.println(die1.roll());            //spiller 2 slår med terninger
+            System.out.println(die2.roll());
+
+            points2.redemption(die1.getFaceValue(), die2.getFaceValue());          //lægger øjne til samlede points
+
+            System.out.println(Player2.Navn + " Har " + points2.points + " points.");       //printer akkumulerede points
+        }
+
+
+        if (points1.points > points2.points) {
+            System.out.println("Tillykke " + Player1.Navn + "! Du har vundet!");
+        } else {
+            System.out.println("Tillykke " + Player2.Navn + "! Du har vundet!");
         }
 
     }
--- src/spil/SpilStart.java
+++ src/spil/SpilStart.java
@@ -3,21 +3,24 @@ import javax.sound.midi.SysexMessage;
 import java.util.Scanner;
 public class SpilStart {
     public static int mont;
+    private String tast = "q";
     public String Spiller1Navn,Spiller2Navn;
-
+    Scanner input = new Scanner(System.in);
     public void names () {
-        Scanner input = new Scanner(System.in);
+
         System.out.println("Skriv navn på spiller 1");
         Spiller1Navn = input.nextLine();
         System.out.println("Skriv navn på spiller 2");
         Spiller2Navn = input.nextLine();
     }
     public void fiftyFifty() {
+        while (tast != "") {
+            System.out.println("Tryk enter for tilfældigt at vælge hvem der starter");
+            tast = input.nextLine();
+        }
         mont = (int) Math.round(Math.random());
         if (mont == 0) {
             System.out.println("Spiller 1 starter");
-        }
-        else System.out.println("Spiller 2 starter");
-
+        } else System.out.println("Spiller 2 starter");
     }
 }
